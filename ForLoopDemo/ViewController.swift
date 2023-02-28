0b4b28f82f099ee2067bb092221b16b4be1d22d0
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-28 23:42:29 +0600

    Update ViewController.swift

--- ForLoopDemo/ViewController.swift
+++ ForLoopDemo/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var nameArray = ["Joynal", "Robin", "Humayun", "Anup", "Murad"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //loop_1
+        for i in 0..<nameArray.count {
+            debugPrint(nameArray[i])
+        }
+        
+        //loop_2
+        for data in nameArray {
+            debugPrint(data)
+        }
+        
+        //loop_3
+        nameArray.forEach{
+            debugPrint($0)
+        }
+        
+        //loop_4
+        (0..<nameArray.count).forEach{
+            debugPrint([$0])
+        }
+        
     }
-
-
 }
 
