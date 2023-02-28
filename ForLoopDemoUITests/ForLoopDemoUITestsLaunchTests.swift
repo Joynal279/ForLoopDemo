69747523a61b249fa33f06115be3626bba54281e
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-28 23:21:17 +0600

    Initial Commit

---
+++ ForLoopDemoUITests/ForLoopDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ForLoopDemoUITestsLaunchTests.swift
+//  ForLoopDemoUITests
+//
+//  Created by Joynal Abedin on 28/2/23.
+//
+
+import XCTest
+
+final class ForLoopDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
